607c9c4919572d58cf7c9d952a22de20f45db2d1
mac <mac@esco-3.local>
2022-03-08 20:51:02 +0000

    last commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import "./app.styles.css";
+import { useEffect } from "react";
 
 const App = () => {
+  useEffect(() => {
+    fetch("/data/data.json")
+      .then((response) => response.json())
+      .then((result) => console.log(result["shoes"]));
+  }, []);
   return (
     <div>
       <h1 className="title">Escowear</h1>
